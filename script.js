33a7bb11e0bcadc9de7f68757d6eb02affdd1a83
anshikaurmaliya <144269806+anshikaurmaliya@users.noreply.github.com>
2024-04-21 20:29:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,189 @@
+function calling(){
+var one = document.querySelector("#one")
+var two = document.querySelector("#two")
+var three = document.querySelector("#three")
+var four = document.querySelector("#four")
+var five = document.querySelector("#five")
+var img = document.querySelector(".beta2")
+document.querySelector("#one").addEventListener("click",function(){
+    img.innerHTML=`<img src="https://images.unsplash.com/photo-1682695797221-8164ff1fafc9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8" alt="">
+    `
+    one.style.height=("fit-content")
+    two.style.height=("10%")
+    three.style.height=("10%")
+    four.style.height=("10%")
+    five.style.height=("10%")
+
+
+
+})
+document.querySelector("#two").addEventListener("click",function(){
+    img.innerHTML=`<img src="https://images.unsplash.com/photo-1698222476261-c84a34b90ca7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D" alt="">
+    `
+    two.style.height=("fit-content")
+    one.style.height=("10%")
+    three.style.height=("10%")
+    four.style.height=("10%")
+    five.style.height=("10%")
+
+})
+document.querySelector("#three").addEventListener("click",function(){
+    img.innerHTML=`<img src="https://images.unsplash.com/photo-1682687220795-796d3f6f7000?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D" alt="">
+    `
+    three.style.height=("fit-content")
+    two.style.height=("10%")
+    one.style.height=("10%")
+    four.style.height=("10%")
+    five.style.height=("10%")
+
+})
+document.querySelector("#four").addEventListener("click",function(){
+    img.innerHTML=`<img src="https://plus.unsplash.com/premium_photo-1697139065201-50c05c556fd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D" alt="">
+    `
+    four.style.height=("fit-content")
+    two.style.height=("10%")
+    three.style.height=("10%")
+    one.style.height=("10%")
+    five.style.height=("10%")
+
+})
+document.querySelector("#five").addEventListener("click",function(){
+    img.innerHTML=`<img src="https://plus.unsplash.com/premium_photo-1702400311707-efad47a8d231?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2Nnx8fGVufDB8fHx8fA%3D%3D" alt="">
+    `
+    five.style.height=("fit-content")
+    two.style.height=("10%")
+    three.style.height=("10%")
+    four.style.height=("10%")
+    one.style.height=("10%")
+
+})
+ var a = document.querySelector("#a");
+ var b = document.querySelector("#b");
+ var c = document.querySelector("#c");
+ var d = document.querySelector("#d");
+ var e = document.querySelector("#e");
+ var f = document.querySelector("#f");
+ var g = document.querySelector("#g");
+ var mem = document.querySelector("#mem");
+//  var a = document.querySelector("a");
+document.querySelector("#cs").addEventListener("click",function(){
+    a.style.height ="fit-content"
+    b.style.height="6%"
+    c.style.height="6%"
+    d.style.height="6%"
+    e.style.height="6%"
+    f.style.height="6%"
+    g.style.height="6%"
+    mem.innerHTML=` <img src="https://plus.unsplash.com/premium_photo-1661962966199-1c6b3041bdef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHNoaXBzJTIwb24lMjB0aGUlMjBzZWF8ZW58MHx8MHx8fDA%3D" alt="">`
+    
+
+
+
+})
+document.querySelector("#ds").addEventListener("click",function(){
+    b.style.height ="fit-content"
+    a.style.height="6%"
+    c.style.height="6%"
+    d.style.height="6%"
+    e.style.height="6%"
+    f.style.height="6%"
+    g.style.height="6%"
+    mem.innerHTML=` <img src="https://images.unsplash.com/photo-1534224039826-c7a0eda0e6b3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGdlbyUyMHRoZXJtYWwlMjBlbmVyZ3l8ZW58MHx8MHx8fDA%3D" alt="">`
+    
+
+
+
+})
+document.querySelector("#aiml").addEventListener("click",function(){
+    c.style.height ="fit-content"
+    a.style.height="6%"
+    b.style.height="6%"
+    d.style.height="6%"
+    e.style.height="6%"
+    f.style.height="6%"
+    g.style.height="6%"
+    mem.innerHTML=`<img src="https://images.unsplash.com/photo-1511993226957-cd166aba52d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZvb2QlMjBwcm9jZXNzaW5nfGVufDB8fDB8fHww" alt="">`
+    
+
+
+
+})
+document.querySelector("#me").addEventListener("click",function(){
+    d.style.height ="fit-content"
+    a.style.height="6%"
+    b.style.height="6%"
+    c.style.height="6%"
+    e.style.height="6%"
+    f.style.height="6%"
+    g.style.height="6%"
+    mem.innerHTML=`<img src="https://media.istockphoto.com/id/922601466/photo/engineering-is-use-notebook-check-and-standing-in-front-of-oil-refinery-building-structure-in.webp?b=1&s=170667a&w=0&k=20&c=0gUrdeoJokiuiNsMJQxzpd6dWpDQsE9GH6W__QozqZE=" alt="">`
+    
+
+
+
+})
+document.querySelector("#cv").addEventListener("click",function(){
+    e.style.height ="fit-content"
+    a.style.height="6%"
+    b.style.height="6%"
+    d.style.height="6%"
+    c.style.height="6%"
+    f.style.height="6%"
+    g.style.height="6%"
+    mem.innerHTML=`<img src="https://images.unsplash.com/photo-1495774539583-885e02cca8c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0ZXIlMjBkZXN0aWxhdGlvbnxlbnwwfHwwfHx8MA%3D%3D" alt="">`
+    
+
+
+
+})
+document.querySelector("#bs").addEventListener("click",function(){
+    f.style.height ="fit-content"
+    a.style.height="6%"
+    b.style.height="6%"
+    d.style.height="6%"
+    e.style.height="6%"
+    c.style.height="6%"
+    g.style.height="6%"
+    mem.innerHTML=`<img src="https://media.istockphoto.com/id/1194430863/photo/smart-female-it-programer-working-on-desktop-computer-in-data-center-system-control-room-team.webp?b=1&s=170667a&w=0&k=20&c=GHSpcQycJtfKQLo1SbK-Z-FaaMzNPAY2JkJCgTfxGqM=" alt="">`
+    
+
+
+
+})
+document.querySelector("#gk").addEventListener("click",function(){
+    g.style.height ="fit-content"
+    a.style.height="6%"
+    b.style.height="6%"
+    d.style.height="6%"
+    e.style.height="6%"
+    f.style.height="6%"
+    c.style.height="6%"
+    mem.innerHTML=`<img src="https://media.istockphoto.com/id/471824724/photo/factory-industrial-plant.webp?b=1&s=170667a&w=0&k=20&c=BsOjqkAQ9Ho5g2vBt_4-S05hlcV0u34fCptzphoRrHc=" alt="">`
+    
+
+
+
+})
+}
+calling();
+function mono(){
+    var pox = document.querySelector("#pox")
+    var pox1 = document.querySelector("#pox1")
+    var pox2 = document.querySelector("#pox2")
+    var lost2 = document.querySelector(".lost2")
+    pox.addEventListener("mouseenter",function(){
+        lost2.innerHTML=`<img src="https://media.istockphoto.com/id/1402430797/photo/business-3d-tablet-virtual-growth-arrow-financial-graph-on-digital-technology-strategy.webp?b=1&s=170667a&w=0&k=20&c=3BAPqK7NM_WePHNOUaAUsf8x8zIfTwTKHCX1sS7waDY=" alt="">`
+        
+        
+    })
+    pox1.addEventListener("mouseenter",function(){
+        lost2.innerHTML=`<img src="https://plus.unsplash.com/premium_photo-1676673222100-203637d00d94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z3JhcGh8ZW58MHx8MHx8fDA%3D" alt="">`
+        
+    })
+    pox2.addEventListener("mouseenter",function(){
+        lost2.innerHTML=`<img src="https://plus.unsplash.com/premium_photo-1664298994861-c77da4eb0a4b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Z3JhcGh8ZW58MHx8MHx8fDA%3D" alt="">`
+        
+    })
+
+}
+mono();
